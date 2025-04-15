810f11b017b242eb899a1cf60c28d42f45611a13
zemtsow <nickwebtm@gmail.com>
2025-04-15 15:34:08 +0500

    vite config changed by new version

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import {resolve} from "path";
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-		alias: {
-			"@": resolve(__dirname, "src"),
-		},
-	}
+    alias: {
+      '@': resolve(__dirname, 'src')
+    },
+    extensions: ['.js', '.ts', '.jsx', '.tsx']
+  },
+  define: {
+    'process.env': {}
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
 })
